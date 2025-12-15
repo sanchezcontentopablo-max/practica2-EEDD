4838951d62d9fc109426d041294884ab3586bf13
LAPTOP-TLI1SARF\pablo <sanchezcontento.pablo@jgvaldemora.org>
2025-12-15 12:10:40 +0100

    prueba conexion de repositorio

---
+++ src/main/java/org/endes/entities/persona.java
@@ -0,0 +1,19 @@
+package org.endes.entities;
+
+public class persona {
+    private String nombre;
+    private Integer apellido;
+    private String edad;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private String altura;
+    private Double peso;
+
+    public persona createpersona() {
+        persona persona = new persona();
+
+
+        return persona;
+    }
+}
